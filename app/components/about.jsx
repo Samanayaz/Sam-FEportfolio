d6280ecb40cfdf7695e9ed96d1dd9f03b93c67bf
Saman <56182914+Samanayaz@users.noreply.github.com>
2024-12-13 10:06:17 -0600

    push

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -53,7 +53,7 @@ const About = () => {
     return (
 
         <section className='text-white' id='about'>
-            <div className='flex flex-col-reverse md:grid md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 items-start'>
+            <div className='md:grid flex flex-col-reverse md:grid-cols-2 gap-8 py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 items-start'>
                 <Image src="/interface2.png" width={500} height={500} alt='monitor and hands' />
                 <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
                     <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
